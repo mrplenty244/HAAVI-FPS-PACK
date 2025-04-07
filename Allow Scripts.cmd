d26ee96f477416233751f3049c4251b89760bf26
mrplenty244 <scape4goat@gmail.com>
2025-04-07 18:26:29 +0530

    Add files via upload

---
+++ Allow Scripts.cmd
@@ -0,0 +1,28 @@
+@echo off
+:: Request admin privileges
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    echo UAC.ShellExecute "%~s0", "", "", "runas", 1 >> "%temp%\getadmin.vbs"
+    "%temp%\getadmin.vbs"
+    exit /B
+)
+
+if exist "%temp%\getadmin.vbs" del "%temp%\getadmin.vbs"
+pushd "%CD%"
+CD /D "%~dp0"
+
+cls
+:: Allow double-click execution of PowerShell scripts
+reg add "HKCR\Applications\powershell.exe\shell\open\command" /ve /t REG_SZ /d "C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -NoLogo -ExecutionPolicy unrestricted -File \"%%1\"" /f >nul 2>&1
+
+:: Set execution policy to Unrestricted
+reg add "HKCU\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v "ExecutionPolicy" /t REG_SZ /d "Unrestricted" /f >nul 2>&1
+reg add "HKLM\SOFTWARE\Microsoft\PowerShell\1\ShellIds\Microsoft.PowerShell" /v "ExecutionPolicy" /t REG_SZ /d "Unrestricted" /f >nul 2>&1
+
+:: Unblock all files in the current directory
+powershell -Command "Get-ChildItem -Path '%~dp0' -Recurse | Unblock-File"
+
+echo Enabled PowerShell Scripts + Unblocked Files
+pause
+exit
